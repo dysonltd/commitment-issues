0e5f21aac3b69eb3331d1b1238b87f4f806bad7b
Scott <scott.gibb@dyson.com>
2025-07-04 16:43:05 +0100

    Adding MacOS specific

--- src/lib.rs
+++ src/lib.rs
@@ -52,7 +52,8 @@ pub fn include_metadata(_: TokenStream) -> TokenStream {
                 footer: [u8; 4],
             }
 
-            #[link_section = ".metadata"]
+            #[cfg_attr(target_os = "macos", link_section = "__DATA,.metadata")]
+            #[cfg_attr(not(target_os = "macos"), link_section = ".metadata")]
             #[used]
             static METADATA: Metadata =
                     Metadata {
